1f38e7d565cd1f9b1f37e436254f25ceff78a721
AhmedTarek22 <ahmedtarekabdo22@gmail.com>
2025-01-02 06:49:58 +0200

    updates and add localstorage

--- src/Hooks/useFetchData.jsx
+++ src/Hooks/useFetchData.jsx
@@ -5,12 +5,20 @@ const useFetchData = (country) => {
   const [filteredCountries, setFilteredCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
-  let url = "https://restcountries.com/v3.1/all";
-  if (country) {
-    url = `https://restcountries.com/v3.1/name/${country}`;
-  }
 
   useEffect(() => {
+    if (country) {
+      fetchDataFromAPI();
+    } else {
+      fetchDataFromLocalStorage();
+    }
+  }, []);
+
+  const fetchDataFromAPI = () => {
+    let url = "https://restcountries.com/v3.1/all";
+    if (country) {
+      url = `https://restcountries.com/v3.1/name/${country}`;
+    }
     setIsLoading(true);
     fetch(url)
       .then((response) => {
@@ -21,18 +29,30 @@ const useFetchData = (country) => {
       })
       .then((data) => {
         if (country) {
+          //Country page
           setCountriesData(data[0]);
         } else {
+          //Home page
+          localStorage.setItem("countries", JSON.stringify(data));
           setCountriesData(data);
           setFilteredCountries(data);
         }
       })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
+      .catch(() => {
         setIsError(true);
       })
       .finally(() => setIsLoading(false));
-  }, []);
+  };
+
+  const fetchDataFromLocalStorage = () => {
+    const data = JSON.parse(localStorage.getItem("countries"));
+    if (data) {
+      setCountriesData(data);
+      setFilteredCountries(data);
+    } else {
+      fetchDataFromAPI();
+    }
+  };
 
   return {
     countriesData,
